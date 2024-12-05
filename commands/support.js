254b865f0210b5ecef7279f8960edd0c3f17bd06
faccout <faccout41@gmail.com>
2024-12-05 16:32:37 +0300

    support.js

--- commands/support.js
+++ commands/support.js
@@ -40,7 +40,7 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1253655047259160596/6186-developer-bot.gif?ex=6752cf3f&is=67517dbf&hm=2fb358f4578004ae3ed9b41e1d43b27e7e051ddc07deeb38e0dac9bca1960bea&', 
               url: 'https://discord.gg/QHzdUj2VqN'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:** https://spellaexp.my.canva.site/web`)
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1273947742724685875/1277995152543055932/0DC0BBD1-3E1D-4BA5-94E4-FA16355D48CA.jpg?ex=67525e7c&is=67510cfc&hm=1c4853605531cac76209779960779073b907eb19a7955cb3c824ea53e9139f5e&')
             .setTimestamp();
 
