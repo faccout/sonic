2a735402ee61e221e617284848b2a65f0bcb1003
faccout <faccout41@gmail.com>
2024-12-05 16:41:07 +0300

    config.js

--- config.js
+++ config.js
@@ -21,17 +21,17 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["921743618316836875", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  activityType: "NONE",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
   MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
-  embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
+  embedTimeout: 1800,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
 
    // Lavalink Server Details
