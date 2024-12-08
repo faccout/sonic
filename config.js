1b14d2109e73dd09ec749586cc7744a6303d996b
faccout <faccout41@gmail.com>
2024-12-08 18:02:03 +0300

    config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "NODE",
-        host: "lavalink.anantix.network",
-        port: 2335,
-        password: "pg6|(}7fuD_:7d#QQq?9",
+        host: "lavalinkv3-id.serenetia.com",
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false 
