af4f8e3b8b426947cedfd3d85915da5a295301c6
faccout <faccout41@gmail.com>
2024-12-11 17:56:31 +0300

    config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "NODE",
-        host: "lavahatry4.techbyte.host",
-        port: 3000,
-        password: "NAIGLAVA-dash.techbyte.host",
+        host: "lavalink1.albinhakanson.se",
+        port: 1141,
+        password: "albinhakanson.se",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false 
