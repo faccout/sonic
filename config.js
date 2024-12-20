131bcd15676a8b3c2751d4a9d0651d7126125b5f
faccout <faccout41@gmail.com>
2024-12-20 22:21:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "NODE",
-        host: "lavalink.jirayu.net",
-        port: 13592,
-        password: "youshallnotpass",
+        host: "45.89.99.118",
+        port: 8000,
+        password: "winkle@team",
         reconnectTimeout: 10000,
         reconnectTries: Infinity,
         secure: false 
